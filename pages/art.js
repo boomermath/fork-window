d4b4c324606bf57061377124829443fee9f0704e
boomermath <63861512+boomermath@users.noreply.github.com>
2021-07-27 12:20:00 -0400

    Update art.js

--- pages/art.js
+++ pages/art.js
@@ -10,7 +10,7 @@ export default function Art() {
             <h1 className="text-6xl">Animation by toaster cat</h1> <br />
             <h2 className="text-3xl">Click for toast</h2>
             <Image
-                onMouseOver={() => { setImgSrc("/bread_down.gif") }}
+                onMouseEnter={() => { setImgSrc("/bread_down.gif") }}
                 onMouseOut={() => { setImgSrc("/bread_static.png") }}
                 onClick={() => { setImgSrc("/toast.gif") }}
                 src={imgSrc} width="300" height="300"

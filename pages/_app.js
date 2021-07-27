ef356f54f6cb6599b3da5c0e07bfef97ee49f47e
boomermath <boomermath13@gmail.com>
2021-07-27 12:04:53 -0400

    change title

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <title>Home</title>
+        <title>Fork Windows</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200&display=swap" rel="stylesheet" />
